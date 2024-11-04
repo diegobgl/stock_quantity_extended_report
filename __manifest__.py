9862dfd5d862a70335839ecc0c1390eca49ceb53
Diego Gajardo <deskodoo@a2group.cl>
2024-11-04 17:43:30 -0300

    version

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Stock Quantity Extended Report',
-    'version': '14.0.1.0.0',
+    'version': '17.0.1.0.0',
     'summary': 'Extiende el reporte de Inventario a la Fecha para incluir ubicación, tipo de movimiento, y más.',
     'description': 'Módulo para extender el reporte de inventario a la fecha en Odoo, incluyendo información adicional como ubicación, tipo de movimiento, lote/número de serie, y valor total.',
     'author': 'Diego',
