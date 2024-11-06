a696dce52fec73b6b12eec0218bab3ac7d92b3c6
Diego Gajardo <deskodoo@a2group.cl>
2024-11-06 12:21:26 -0300

    cersion completa

--- models/stock_quantity_history.py
+++ models/stock_quantity_history.py
@@ -1,4 +1,4 @@
-from odoo import  _, models, fields, api
+from odoo import models, fields, api, _
 from odoo.tools.misc import format_datetime
 from odoo.osv import expression
 
@@ -26,7 +26,7 @@ class StockQuantityHistory(models.TransientModel):
         }
 
         return action
-    
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
@@ -35,9 +35,48 @@ class ProductProduct(models.Model):
         compute='_compute_location_ids', store=False
     )
 
+    last_move_date = fields.Datetime(
+        string='Fecha Último Movimiento',
+        compute='_compute_last_move_info', store=False
+    )
+
+    move_type = fields.Selection(
+        [('purchase', 'Compra'), ('internal', 'Transferencia Interna')],
+        string='Tipo Movimiento',
+        compute='_compute_last_move_info', store=False
+    )
+
+    valuation_value = fields.Float(
+        string='Valorizado',
+        compute='_compute_valuation_value', store=False
+    )
+
     def _compute_location_ids(self):
         for product in self:
             # Obtener todas las ubicaciones donde se encuentra este producto
             quant_records = self.env['stock.quant'].search([('product_id', '=', product.id)])
             product.location_ids = quant_records.mapped('location_id')
+
+    def _compute_last_move_info(self):
+        for product in self:
+            # Obtener el movimiento más reciente relacionado con el producto
+            last_move = self.env['stock.move'].search(
+                [('product_id', '=', product.id)],
+                order='date desc',
+                limit=1
+            )
+            if last_move:
+                product.last_move_date = last_move.date
+                product.move_type = 'purchase' if last_move.picking_type_id.code == 'incoming' else 'internal'
+            else:
+                product.last_move_date = False
+                product.move_type = False
+
+    def _compute_valuation_value(self):
+        for product in self:
+            # Obtener la cantidad disponible desde `stock.quant`
+            quant_records = self.env['stock.quant'].search([('product_id', '=', product.id)])
+            quantity = sum(quant_records.mapped('quantity'))
+            # Valorización = Cantidad * Costo Unitario
+            product.valuation_value = quantity * product.standard_price
 
