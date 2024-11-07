aec48fb2953a950383e3e060e3652893cbc83176
Diego Gajardo <deskodoo@a2group.cl>
2024-11-07 09:50:19 -0300

    stock valuation layer add

--- models/stock_quantity_history.py
+++ models/stock_quantity_history.py
@@ -2,36 +2,51 @@ from odoo import models, fields, api, _
 from odoo.tools.misc import format_datetime
 from odoo.osv import expression
 
-class StockQuantityHistory(models.TransientModel):
+class StockQuantityHistoryExtended(models.TransientModel):
     _inherit = 'stock.quantity.history'
 
+    location_id = fields.Many2one(
+        'stock.location', string="Ubicación",
+        domain="[('usage', 'in', ['internal', 'transit'])]"
+    )
+
     def open_at_date(self):
-        """
-        Modificación del método `open_at_date` para agrupar primero por ubicación y luego por producto.
-        """
-        # Obtenemos la vista tree de `stock.quant` que contiene información sobre las ubicaciones y productos
-        tree_view_id = self.env.ref('stock.view_stock_quant_tree').id
+        active_model = self.env.context.get('active_model')
 
-        # Definimos la acción para abrir los resultados en la vista tree
-        action = {
-            'type': 'ir.actions.act_window',
-            'views': [(tree_view_id, 'tree')],
-            'view_mode': 'tree',
-            'name': _('Product Quantities by Location'),
-            'res_model': 'stock.quant',
-            'domain': [('product_id.type', '=', 'product')],
-            'context': dict(self.env.context, to_date=self.inventory_datetime),
-            'display_name': format_datetime(self.env, self.inventory_datetime),
-            'group_by': ['location_id', 'product_id'],  # Agrupar primero por ubicación y luego por producto
-        }
+        if active_model == 'stock.valuation.layer':
+            # Obtener la acción original
+            action = self.env["ir.actions.actions"]._for_xml_id("stock_account.stock_valuation_layer_action")
 
-        return action
+            # Configuración de vistas
+            tree_view = self.env.ref('stock_account.stock_valuation_layer_valuation_at_date_tree_inherited', raise_if_not_found=False)
+            graph_view = self.env.ref('stock_account.stock_valuation_layer_graph', raise_if_not_found=False)
+            action['views'] = [
+                (tree_view.id if tree_view else False, 'tree'),
+                (self.env.ref('stock_account.stock_valuation_layer_form').id, 'form'),
+                (self.env.ref('stock_account.stock_valuation_layer_pivot').id, 'pivot'),
+                (graph_view.id if graph_view else False, 'graph')
+            ]
+
+            # Modificar el dominio para incluir `location_id` si está especificado
+            domain = [('create_date', '<=', self.inventory_datetime), ('product_id.type', '=', 'product')]
+            if self.location_id:
+                domain.append(('location_id', '=', self.location_id.id))
+
+            action['domain'] = domain
+            action['display_name'] = format_datetime(self.env, self.inventory_datetime)
+            action['context'] = "{}"
+
+            return action
+
+        # Comportamiento original para otros modelos
+        return super(StockQuantityHistoryExtended, self).open_at_date()
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     location_ids = fields.Many2many(
-        'stock.location', string='Locations',
+        'stock.location', string='Ubicaciones',
         compute='_compute_location_ids', store=False
     )
 
@@ -53,13 +68,11 @@ class ProductProduct(models.Model):
 
     def _compute_location_ids(self):
         for product in self:
-            # Obtener todas las ubicaciones donde se encuentra este producto
             quant_records = self.env['stock.quant'].search([('product_id', '=', product.id)])
             product.location_ids = quant_records.mapped('location_id')
 
     def _compute_last_move_info(self):
         for product in self:
-            # Obtener el movimiento más reciente relacionado con el producto
             last_move = self.env['stock.move'].search(
                 [('product_id', '=', product.id)],
                 order='date desc',
@@ -74,10 +87,8 @@ class ProductProduct(models.Model):
 
     def _compute_valuation_value(self):
         for product in self:
-            # Obtener la cantidad disponible desde `stock.quant`
             quant_records = self.env['stock.quant'].search([('product_id', '=', product.id)])
             quantity = sum(quant_records.mapped('quantity'))
-            # Valorización = Cantidad * Costo Unitario
             product.valuation_value = quantity * product.standard_price
 
 
@@ -102,11 +113,6 @@ class StockQuant(models.Model):
         compute='_compute_valuation_value', store=False
     )
 
-    account_valuation_id = fields.Many2one(
-        'account.account', string='Cuenta Contable de Valorización',
-        compute='_compute_account_valuation', store=False
-    )
-
     weighted_average_price = fields.Float(
         string='Precio Unitario',
         compute='_compute_weighted_average_price', store=False
@@ -114,7 +120,6 @@ class StockQuant(models.Model):
 
     def _compute_last_move_info(self):
         for quant in self:
-            # Obtener el movimiento más reciente relacionado con el producto y ubicación específica del `quant`
             last_move = self.env['stock.move'].search(
                 [('product_id', '=', quant.product_id.id), ('location_dest_id', '=', quant.location_id.id)],
                 order='date desc',
@@ -131,51 +136,48 @@ class StockQuant(models.Model):
         to_date = self.env.context.get('to_date')
         for quant in self:
             product = quant.product_id
-
-            # Inicializar variables para el cálculo del precio promedio ponderado
             total_value = 0.0
             total_quantity = 0.0
 
-            # Ajustar la búsqueda de movimientos para que respete la fecha 'to_date'
-            domain = [
-                ('product_id', '=', product.id),
-                ('state', '=', 'done'),
-            ]
+            domain = [('product_id', '=', product.id), ('state', '=', 'done')]
             if to_date:
-                domain.append(('date', '<=', to_date))  # Limitar la búsqueda hasta la fecha proporcionada
+                domain.append(('date', '<=', to_date))
 
-            # Buscar movimientos de stock hasta la fecha especificada en el contexto
             moves = self.env['stock.move'].search(domain)
-
-            # Calcular el valor y cantidad total de los movimientos, incluyendo ajustes iniciales
             for move in moves:
-                if move.picking_type_id.code in ['incoming', 'inventory']:  # Compras o ajustes de inventario
+                if move.picking_type_id.code in ['incoming', 'inventory']:
                     total_value += move.price_unit * move.product_qty
                     total_quantity += move.product_qty
-                elif move.picking_type_id.code == 'outgoing':  # Salidas o ventas
+                elif move.picking_type_id.code == 'outgoing':
                     total_quantity -= move.product_qty
-
-                # Asegurarse de que la cantidad nunca sea negativa
                 if total_quantity < 0:
                     total_quantity = 0
 
-            # Determinar el precio promedio ponderado o usar el standard_price si no hay datos suficientes
-            if total_quantity > 0:
-                quant.weighted_average_price = total_value / total_quantity
-            else:
-                quant.weighted_average_price = product.standard_price
-
-            # Fallback adicional para asegurar que siempre hay un valor
-            if quant.weighted_average_price <= 0 and product.standard_price > 0:
-                quant.weighted_average_price = product.standard_price
+            quant.weighted_average_price = total_value / total_quantity if total_quantity > 0 else product.standard_price
 
     def _compute_valuation_value(self):
         for quant in self:
-            # Asegurarse de que el precio unitario no sea cero antes de calcular la valorización
             price_unit = quant.weighted_average_price if quant.weighted_average_price > 0 else quant.product_id.standard_price
-            quant.valuation_value = max(quant.quantity, 0) * price_unit  # Asegurarse de que la cantidad no sea negativa
+            quant.valuation_value = max(quant.quantity, 0) * price_unit
 
-    def _compute_account_valuation(self):
         for quant in self:
             # Obtener la cuenta contable de valorización desde la categoría del producto
             quant.account_valuation_id = quant.product_id.categ_id.property_stock_valuation_account_id
+
+
+class StockValuationLayer(models.Model):
+    _inherit = 'stock.valuation.layer'
+
+    location_id = fields.Many2one(
+        'stock.location', string="Ubicación",
+        compute='_compute_location_id', store=True
+    )
+
+    @api.depends('stock_move_id')
+    def _compute_location_id(self):
+        for layer in self:
+            # Usar location_id del movimiento de stock asociado, si está disponible
+            if layer.stock_move_id:
+                layer.location_id = layer.stock_move_id.location_id
+            else:
+                layer.location_id = False
